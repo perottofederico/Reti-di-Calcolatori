9a7b7ce50476388da24af3309418ac4751e6e23b
Siopedo <perotto.1701738@studenti.uniroma1.it>
2018-12-16 17:43:51 +0100

    switched view engine to ejs and implemented omdb

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path= require('path');
+var request = require('request');
 
 router.use(function timeLog(req, res, next){
 	console.log('Time: ', Date.now());
@@ -8,19 +9,41 @@ router.use(function timeLog(req, res, next){
 });
 
 router.get('/', function(req, res){
-	res.sendFile(path.resolve(__dirname+'/../views/index.html'));
+	res.render(path.resolve(__dirname+'/../views/index.ejs'));
 });
 
 router.get('/login', function(req, res){
-	res.sendFile(path.resolve(__dirname+'/../views/login.html'));
+	res.render(path.resolve(__dirname+'/../views/login.ejs'));
 });
 
-router.get('/ricerca', function(req, res){
-	res.sendFile(path.resolve(__dirname+'/../views/ricerca.html'));
+
+
+var film; //Variable including movie info
+router.get('/search', function(req, res){
+	res.render(path.resolve(__dirname+'/../views/search.ejs'));
 });
+
+router.post('/search', function(req, res){
+	var options = {url: 'http://www.omdbapi.com/?apikey=2fab0a6a&t='+ req.body.name};
+	request.get(options, function(error, response, body){
+		if(!error && response.statusCode == 200){
+			film = JSON.parse(body);
+			console.log(film);
+			res.redirect('/film');
+		}
+	});
+});
+
+router.get('/film', function(req, res){
+	res.render(path.resolve(__dirname+'/../views/film.ejs'), {
+		title:film.Title,
+		poster:film.Poster
+	});
+});
+
 
 router.get('/visti', function(req, res){
-	res.sendFile(path.resolve(__dirname+'/../views/visti.html'));
+	res.render(path.resolve(__dirname+'/../views/visti.ejs'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const path = require('path'); //Required to work with directories
 const methods = require('methods'); //Lowercase http methods and more.
 const bodyParser = require('body-parser'); //Middleware to parse the req.body property, before handlers.
 const session = require('express-session'); //Creates a middleware for the session, with the given options.
-const cors = require('cors'); //Cross-origin resource sharing (Do i need this?)
+//const cors = require('cors'); Cross-origin resource sharing (Do i need this?)
 const passport = require('passport'); //Autentication middleware. Required for oAuth.
 const errorHandler = require('errorhandler'); //Error handler for development environment.
 //const mongoose = require('mongoose'); MongoDB. Maybe will use couchDB
 const morgan = require('morgan'); //HTTP request logger middleware
 var routes = require('./routes');
 
-const views = __dirname+'/views'; //Directory for html files. Will probably move when routes are set up
 const port = process.env.PORT || 3000; //Server port
 
 
@@ -25,9 +24,12 @@ var app = express();
 //Express config defaults
 //app.use(cors());
 app.use(morgan('dev'));
-app.use('/', routes);
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(bodyParser.json());		//To get content of forms
+
+app.set('view engine', 'ejs');
+app.use('/', routes);
+
 
 //app.use(require('method-override')());
 app.use(express.static(__dirname+'/public')); //Load static files in public folder
