d586a43e0c1b9ee184077913900faf7ed142c1a1
Siopedo <perotto.1701738@studenti.uniroma1.it>
2018-12-19 01:14:56 +0100

    removed file containing client ID and secret ID

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'googleAuth' : {
-		'ClientID' : '127288589065-9ft2e6kr79iblq1k0sahbddm2mc4pef6.apps.googleusercontent.com',
-		'ClientSecret' : '6VDznXT9RfYhaBBFaPBEFltw',
-		'CallbackURL' : 'http://localhost:3000/auth/google/callback'
-	}
-}
